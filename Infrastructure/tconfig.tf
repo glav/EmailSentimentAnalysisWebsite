7ff90004706c96faa972b0c5204e55a3aa14400b
Paul Glavich <glav@theglavs.com>
2018-12-11 15:49:27 +1100

    Removed infra back to main analysis tenant

--- Infrastructure/tconfig.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "GeneralStorage"
-    storage_account_name = "terraformstore"
-    container_name       = "deploystatesentimentwebsite"
-    key                  = "devwebsite.terraform.tfstate"
-  }
-}
